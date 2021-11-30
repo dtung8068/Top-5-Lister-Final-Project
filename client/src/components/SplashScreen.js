78ce6fc3006b5544f7065c6342a9bb074c47d803
Scr0ll0 <56087882+Scr0ll0@users.noreply.github.com>
2021-11-30 14:19:46 -0500

    Splash Screen Progress.

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,8 +1,12 @@
 export default function SplashScreen() {
     return (
         <div id="splash-screen">
-            The Top 5<br />
-            Lister
+            <h1>Welcome To The Top 5 Lister</h1>
+            <h2> 
+            Create Your Own Top 5 Lists! <br />
+            Share Your Lists With Others! <br />
+            See What The Community Thinks!
+            </h2>
         </div>
     )
 }
